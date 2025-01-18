6ad13b91e3c4aab4a90bfc9a0f568060180556e4
Omar <Omar@DESKTOP-OUKQKJG>
2025-01-18 04:25:14 +0300

    First Video

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Sec5
+﻿using Sec5.abstraction;
+
+namespace Sec5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region First Video 
+            //Shape shape;
+
+            /////
+            //Rect rect = new Rect() { Dim01 = 10, Dim02 = 20 };
+            //decimal rectarea = rect.CalcArea();
+            //decimal rectPeri = rect.Perimeter;
+
+            //Console.WriteLine(rectPeri);
+            //Console.WriteLine(rectarea);
+
+
+            //Square square = new Square(20);
+            //decimal squarePeri = square.Perimeter;
+            //decimal squareArea = square.CalcArea();
+            //Console.WriteLine(squarePeri);
+            //Console.WriteLine(squareArea);
+
+
+            //Circle circle = new Circle(10);
+            //decimal circlePeri = circle.Perimeter;
+            //decimal circleArea = circle.CalcArea();
+            //Console.WriteLine(circlePeri);
+            //Console.WriteLine(circleArea); 
+            #endregion
+
+
         }
     }
 }
---
+++ abstraction/Shape.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec5.abstraction
+{
+     abstract class Shape
+    {
+        public decimal Dim01 { get; set; }
+        public decimal Dim02 { get; set; }
+
+        public abstract decimal Perimeter { get;  }
+        public abstract decimal CalcArea();
+        
+    }
+
+
+    class Rect : RectBase
+    {
+        public override decimal Perimeter { 
+            get { return (Dim01 + Dim02) * 2; }
+        }
+     
+
+
+    }
+
+    abstract class RectBase : Shape
+    {
+        public override decimal CalcArea()
+        {
+            return Dim02 * Dim01;
+        }
+
+    }
+
+    class Parent
+    {
+        public virtual int Salary { get; set; }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("I am Parent");
+        }
+    }
+
+    class Child : Parent
+    {
+        public override int Salary {
+            get { return base.Salary; }
+            set { base.Salary = value +2000; }
+        }
+        public override void Print()
+        {
+            base.Print();
+        }
+    }
+
+    class Square : RectBase
+    {
+
+        public Square(int Dim)
+        {
+            this.Dim01 = this.Dim02 = Dim;
+        }
+        public override decimal Perimeter {
+            get
+            {
+                return Dim01 * 4;
+            }             }
+       
+    }
+
+    class Circle : Shape
+    {
+        public Circle(decimal Radius)
+
+        {
+            this.Dim01 = this.Dim02 = Radius;
+            
+        }
+        public override decimal Perimeter {
+
+            get { return 2 * 3.14m * this.Dim02; }        
+        }
+
+        public override decimal CalcArea()
+        {
+            return 3.14m * this.Dim01 * this.Dim01;
+        }
+    }
+}
