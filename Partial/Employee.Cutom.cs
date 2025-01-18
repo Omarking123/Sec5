2bca799a3080e409e1f471215ccbf40495543af7
Omar <Omar@DESKTOP-OUKQKJG>
2025-01-18 07:45:14 +0300

    vIdeo 5

---
+++ Partial/Employee.Cutom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sec5.abstraction;
+
+namespace Sec5.Partial
+{
+    internal partial class Employee:Parent
+    {
+        public string? Address { get; set; }
+
+        public  void DoSomeCode(int X)
+        {
+            int x = 10;
+            x++;
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Partial/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sec5.abstraction;
+
+namespace Sec5.Partial
+{
+    internal partial class Employee : Parent
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public int? Age { get; set; }
+        partial  void DoSomeCode();
+        public void Test()
+        {
+            DoSomeCode();
+        }
+    }
+}
